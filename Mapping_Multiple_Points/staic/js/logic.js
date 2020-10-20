d729897dc7b44cfca606aa72c8f1a107d79d2356
Peter Miller <peteremiller@outlook.com>
2020-10-20 15:21:43 -0500

    Updating files.

--- Mapping_Multiple_Points/staic/js/logic.js
+++ Mapping_Multiple_Points/staic/js/logic.js
@@ -1,15 +1,18 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
+//Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 18);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "white",
-    fillColor: "red"
-}).addTo(map);
+// //Add a marker to the map for Los Angeles, California.
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "white",
+//     fillColor: "yellow"
+// }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
